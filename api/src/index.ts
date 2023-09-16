40af55d986933760a7084012c1a66ab688dd7a71
Caleb Rivera <calebrossr@gmail.com>
2023-09-16 14:16:22 -0400

    bootstrap api

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
